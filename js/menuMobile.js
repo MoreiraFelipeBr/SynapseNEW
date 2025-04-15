0435c97a1738bc9607bb86310202ba2ab009620d
Felipe Moreira <164668169+MoreiraFelipeBr@users.noreply.github.com>
2025-04-15 19:06:49 -0300

    menu

--- js/menuMobile.js
+++ js/menuMobile.js
@@ -9,3 +9,26 @@
 //     }
 //     console.log("cliquei")
 // }
+
+function menuShow() {
+    const menuMobile = document.querySelector('.mobile-menu');
+    const menuIcon = document.querySelector('.mobile-menu-icon .icon');
+    
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        menuIcon.src = "img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        menuIcon.src = "img/close_white_36dp.svg";
+    }
+}
+
+// Fechar o menu ao clicar nos links
+document.querySelectorAll('.mobile-menu .nav-item a').forEach(item => {
+    item.addEventListener('click', () => {
+        const menuMobile = document.querySelector('.mobile-menu');
+        if (menuMobile.classList.contains('open')) {
+            menuShow();
+        }
+    });
+});
